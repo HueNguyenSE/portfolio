81c5bf2455173b6f6be326a0599d4acb74fb3971
HueNguyen <nguyenhue.se@gmail.com>
2023-03-16 14:32:14 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `sophie portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
